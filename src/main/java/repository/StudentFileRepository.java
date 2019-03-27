e2a2e16379413f9afaf5978568c8fd97bc29faca
Banciu Raul <raul_banciu@yahoo.com>
2019-03-27 10:54:16 +0200

    asfed

--- src/main/java/repository/StudentFileRepository.java
+++ src/main/java/repository/StudentFileRepository.java
@@ -10,7 +10,7 @@ public class StudentFileRepository extends AbstractFileRepository<String, Studen
 
     public StudentFileRepository(Validator<Student> validator, String filename) {
         super(validator, filename);
-        loadFromFile();
+        //loadFromFile();
     }
 
     protected void loadFromFile() {
@@ -30,26 +30,26 @@ public class StudentFileRepository extends AbstractFileRepository<String, Studen
     }
 
     protected void writeToFile(Student student) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
-            bw.write(student.getID() + "#" + student.getNume() + "#" + student.getGrupa() + "\n");
-        }
-        catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
+//        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
+//            bw.write(student.getID() + "#" + student.getNume() + "#" + student.getGrupa() + "\n");
+//        }
+//        catch(IOException ioe) {
+//            ioe.printStackTrace();
+//        }
     }
 
     protected void writeToFileAll() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, false))) {
-            super.entities.values().forEach(student -> {
-                try {
-                    bw.write(student.getID() + "#" + student.getNume() + "#" + student.getGrupa() + "\n");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
+//        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, false))) {
+//            super.entities.values().forEach(student -> {
+//                try {
+//                    bw.write(student.getID() + "#" + student.getNume() + "#" + student.getGrupa() + "\n");
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//        catch(IOException ioe) {
+//            ioe.printStackTrace();
+//        }
     }
 }
--- src/main/java/repository/TemaFileRepository.java
+++ src/main/java/repository/TemaFileRepository.java
@@ -10,46 +10,46 @@ public class TemaFileRepository extends AbstractFileRepository<String, Tema> {
 
     public TemaFileRepository(Validator<Tema> validator, String filename) {
         super(validator, filename);
-        loadFromFile();
+        //loadFromFile();
     }
 
     protected void loadFromFile() {
-        try (BufferedReader buffer = new BufferedReader(new FileReader(filename))) {
-            buffer.lines().collect(Collectors.toList()).forEach(line -> {
-                String[] result = line.split("#");
-                Tema tema = new Tema(result[0], result[1], Integer.parseInt(result[2]), Integer.parseInt(result[3]));
-                try {
-                    super.save(tema);
-                } catch (ValidationException ve) {
-                    ve.printStackTrace();
-                }
-            });
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+//        try (BufferedReader buffer = new BufferedReader(new FileReader(filename))) {
+//            buffer.lines().collect(Collectors.toList()).forEach(line -> {
+//                String[] result = line.split("#");
+//                Tema tema = new Tema(result[0], result[1], Integer.parseInt(result[2]), Integer.parseInt(result[3]));
+//                try {
+//                    super.save(tema);
+//                } catch (ValidationException ve) {
+//                    ve.printStackTrace();
+//                }
+//            });
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
     }
 
     protected void writeToFile(Tema tema) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
-            bw.write(tema.getID() + "#" + tema.getDescriere() + "#" + tema.getDeadline() + "#" + tema.getStartline() + "\n");
-        }
-        catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
+//        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
+//            bw.write(tema.getID() + "#" + tema.getDescriere() + "#" + tema.getDeadline() + "#" + tema.getStartline() + "\n");
+//        }
+//        catch(IOException ioe) {
+//            ioe.printStackTrace();
+//        }
     }
 
     protected void writeToFileAll() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, false))) {
-            super.entities.values().forEach(tema -> {
-                try {
-                    bw.write(tema.getID() + "#" + tema.getDescriere() + "#" + tema.getDeadline() + "#" + tema.getStartline() + "\n");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
+//        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, false))) {
+//            super.entities.values().forEach(tema -> {
+//                try {
+//                    bw.write(tema.getID() + "#" + tema.getDescriere() + "#" + tema.getDeadline() + "#" + tema.getStartline() + "\n");
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//        catch(IOException ioe) {
+//            ioe.printStackTrace();
+//        }
     }
 }
